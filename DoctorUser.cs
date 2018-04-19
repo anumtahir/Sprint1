21e10dd56895bd95f2c74e6b3deedefbe2d4689e
QuratulAin <kashafjamal18@hotmail.com>
2018-04-19 10:56:49 +0500

    huh

---
+++ DoctorUser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfService4
+{
+    public class DoctorUser
+    {
+        private string doctorname;
+        private string doctorCNIC;
+        private string hospitalname;
+        private string doctordate;
+
+        public string Doctorname
+        {
+            get { return doctorname; }
+            set { doctorname = value; }
+        }
+        public string DoctorCNIC
+        {
+            get { return doctorCNIC; }
+            set { doctorCNIC = value; }
+        }
+        public string Hospitalname
+        {
+            get { return hospitalname; }
+            set { hospitalname = value; }
+        }
+        public string Doctordate
+        {
+            get { return doctordate; }
+            set { doctordate = value; }
+        }
+        
+    }
+}
